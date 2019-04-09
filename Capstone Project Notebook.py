0ccdab6405b9f5e79f0362758c3af10ddf0bdcaf
David Simon <davidsimon025@gmail.com>
2019-04-08 18:09:46 -0700

    First Part of Data Science Capstone Project

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# # Capstone Project Notebook
+
+# *This notebook will mainly be used to generate code for the Capstone Project in the IBM Data Science course*
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
